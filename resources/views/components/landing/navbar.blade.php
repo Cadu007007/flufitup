46b50c87e07e8d837eb258ca0bb43fee1a10e0ae
Mohamed Salah <sala7thedeveloper101@gmail.com>
2020-09-16 20:34:30 +0200

    semi-finished landing page for desktop

---
+++ resources/views/components/landing/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="navbar-container">
+    <div class="navbar-item">
+        Link 3
+    </div>
+    <div class="navbar-item">
+        Link 2
+    </div>
+    <div class="navbar-item">
+        Link 1
+    </div>
+    <div class="navbar-item logo">
+        <img class="icon" src="{{ asset('images/logo.png') }}" alt="">
+    </div>
+    <div class="navbar-item">
+        Link 3
+    </div>
+    <div class="navbar-item">
+        Link 2
+    </div>
+    <div class="navbar-item">
+        Link 1
+    </div>
+</div>
