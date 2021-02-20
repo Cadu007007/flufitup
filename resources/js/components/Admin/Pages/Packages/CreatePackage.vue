04f8f22b3c9362871fb4672d462399773c3ed7fa
Mohamed Salah <sala7thedeveloper101@gmail.com>
2021-02-20 11:37:27 +0200

    add options to create package view

--- resources/js/components/Admin/Pages/Packages/CreatePackage.vue
+++ resources/js/components/Admin/Pages/Packages/CreatePackage.vue
@@ -4,54 +4,321 @@
             <p class="title">{{ title }}</p>
             <p class="date">{{ date }}</p>
         </div>
-        <div class="package-title-div flex-row" style="margin-bottom: 20px">
-            <p class="title">Package Category</p>
-            <select class="category-select" name="category_id" id="">
-                <option selected>Choose a category</option>
-                <option value="1">Ad Hoc</option>
-                <option value="2">Bi‐Weekly</option>
-                <option value="3">Monthly</option>
-            </select>
-        </div>
+        <br />
+        <form action="/dummy" method="post">
 
-        <div class="flex-row">
-            <div class="flex-column">
-                <div class="package-title-div">
-                    <p class="title">Package Name</p>
-                    <input
-                        class="package-name"
-                        type="text"
-                        name="name"
-                        id=""
-                        placeholder="Package Name"
-                        v-model="name"
-                    />
-                    <span class="delete" @click="clearPackageName">
-                        <img
-                            src="/images/admin/icons/notification-cancel.svg"
-                            alt=""
+            <input type="hidden" :value="csrf" name="_token" />
+
+            <div
+                class="package-title-div flex-column"
+                style="margin-bottom: 20px"
+            >
+                <p class="title">Package Category</p>
+                <select
+                    class="select2"
+                    name="category_id"
+                    id=""
+                    style="width: 300px"
+                >
+                    <option selected>Choose a category</option>
+                    <option value="1">Ad Hoc</option>
+                    <option value="2">Bi‐Weekly</option>
+                    <option value="3">Monthly</option>
+                </select>
+            </div>
+
+            <div class="flex-row">
+                <div class="flex-column">
+                    <div class="package-title-div">
+                        <p class="title">Package Name</p>
+                        <input
+                            class="package-name"
+                            type="text"
+                            name="name"
+                            id=""
+                            placeholder="Package Name"
+                            v-model="name"
                         />
-                    </span>
+                        <span class="delete" @click="clearPackageName">
+                            <img
+                                src="/images/admin/icons/notification-cancel.svg"
+                                alt=""
+                            />
+                        </span>
+                    </div>
+
+                    <div class="package-features">
+                        <AddPackageItem
+                            v-for="(item, index) in items"
+                            :key="index"
+                            :label="item.label"
+                            :data="item.data"
+                            @delete-item="deleteItem(index)"
+                        />
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            No. of Pickups Per Week
+                        </p>
+                        <select
+                            name="number_of_pickups"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">1 Pickup</option>
+                            <option value="2">2 Pickup</option>
+                            <option value="3">3 Pickup</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Advance Notice
+                        </p>
+                        <select
+                            name="advance_notice"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">1 Day</option>
+                            <option value="2">2 Days</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Allowed Bags per Pickup
+                        </p>
+                        <select
+                            name="allowed_bags_per_pickup"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">1 Bag</option>
+                            <option value="2">2 Bags</option>
+                            <option value="3">3 Bags</option>
+                            <option value="4">4 Bags</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Return Service
+                        </p>
+                        <select
+                            name="return_service"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">12 Hours</option>
+                            <option value="2">24 Hours</option>
+                            <option value="3">48 Hours</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Washing Options
+                        </p>
+                        <select
+                            name="washing_option"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">Machine Wash</option>
+                            <option value="2">Hand Wash</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Drying Options
+                        </p>
+                        <select
+                            name="drying_option"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">Tumble Dry</option>
+                            <option value="2">Air Dry</option>
+                            <option value="3">Air Dry - Flat</option>
+                        </select>
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Folding Options
+                        </p>
+                        <select
+                            name="folding_option"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">Folding</option>
+                            <option value="2">Hanger Per Bag</option>
+                            <option value="3">Hanger Per Piece</option>
+                        </select>
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Laundry Detergent
+                        </p>
+                        <select
+                            name="laundry_detergent"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">Standard Line</option>
+                            <option value="2">Special LineBag</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Laundry Softener
+                        </p>
+                        <select
+                            name="laundry_softener"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">Standard Line</option>
+                            <option value="2">Special LineBag</option>
+                        </select>
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Scent Booster
+                        </p>
+                        <select
+                            name="scent_booster"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">None</option>
+                            <option value="2">Standard Line</option>
+                            <option value="3">Special LineBag</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Fabric Fresheners
+                        </p>
+                        <select
+                            name="fabric_fresheners"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">None</option>
+                            <option value="2">Standard Line</option>
+                            <option value="3">Special LineBag</option>
+                        </select>
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Dryer Sheets
+                        </p>
+                        <select
+                            name="dryer_sheet"
+                            id=""
+                            class="select2"
+                            style="width:300px; margin-top: 20px"
+                        >
+                            <option value="1">None</option>
+                            <option value="2">1 Sheet</option>
+                            <option value="3">Multiple Per Bag</option>
+                        </select>
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Dry Clean Credit
+                        </p>
+                        <input
+                            name="dry_clean_credit"
+                            class="add-package-value"
+                            placeholder="Dry Clean Credit"
+                            type="number"
+                            min="0"
+                        />
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            House Hold Credit
+                        </p>
+                        <input
+                            name="house_hold_credit"
+                            class="add-package-value"
+                            placeholder="House Hold Credit"
+                            type="number"
+                            min="0"
+                        />
+                    </div>
+
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Added Value Services Credit
+                        </p>
+                        <input
+                            name="added_value_services_credit"
+                            class="add-package-value"
+                            placeholder="Added Value Services Credit"
+                            type="number"
+                            min="0"
+                        />
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Rewards Points
+                        </p>
+                        <input
+                            name="reward_points"
+                            class="add-package-value"
+                            placeholder="Rewards Points"
+                            type="number"
+                            min="0"
+                        />
+                    </div>
+                    <div class="" style="margin-bottom: 20px">
+                        <p class="title" style="margin-bottom: 10px">
+                            Price of Extra Pound
+                        </p>
+                        <input
+                            name="price_for_extra_pound"
+                            class="add-package-value"
+                            placeholder="Price of Extra Pound"
+                            type="number"
+                            min="0"
+                        />
+                    </div>
                 </div>
 
-                <div class="package-features">
-                    <AddPackageItem
-                        v-for="(item, index) in items"
-                        :key="index"
-                        :label="item.label"
-                        :data="item.data"
-                        @delete-item="deleteItem(index)"
+                <div class="price-card-container">
+                    <CreatePackagePriceCard
+                        currency="$"
+                        price="0"
+                        pricename="package_price"
                     />
                 </div>
             </div>
-            <div class="price-card-container">
-                <CreatePackagePriceCard currency="$" price="0" />
-            </div>
-        </div>
+        </form>
 
-        <p class="add-new-item" @click="addNewItemContainer()">
+        <!-- <p class="add-new-item" @click="addNewItemContainer()">
             Add Another Feature
-        </p>
+        </p> -->
     </div>
 </template>
 <style lang="scss">
@@ -126,6 +393,16 @@ $blue: #22aee4;
         font-family: "Open-Sans-Bold";
         font-size: 14px;
     }
+
+    .add-package-value {
+        width: 312px;
+        height: 45px;
+        background: #f9f9f9;
+        padding: 13px 24px;
+        border: 1px solid #ededed;
+        border-radius: 7px;
+        margin: 6px 0;
+    }
 }
 </style>
 <script>
@@ -138,7 +415,9 @@ export default {
     data() {
         return {
             name: "",
-            items: [{ label: "", data: "" }]
+            csrf: document
+                .querySelector('meta[name="csrf-token"]')
+                .getAttribute("content")
         };
     },
     components: {
