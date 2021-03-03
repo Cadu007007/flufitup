65db8d427d828f5856e55b6e615e02a463fc01b2
Mohamed Salah <sala7thedeveloper101@gmail.com>
2021-03-03 22:18:17 +0200

    edit package blade

--- resources/views/admin/packages/edit.blade.php
+++ resources/views/admin/packages/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin')
 @section('content')
+{{ dd($package) }}
 <Edit-Package
 title="Edit Package"
 date="{{Carbon\Carbon::now()->format('D, d F')}}"
 :addedvalues="{{$added_values}}"
 :package="{{$package}}"
-{{-- updatepackageroute="{{ route('admin.packages.update', 'package_id') }}" --}}
+updatepackageroute="{{ route('admin.packages.update', 'package_id') }}"
 />
 @endsection
