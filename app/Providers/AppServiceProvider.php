3718f7800d382b6469a5cb98501a340b253f844a
Amr Hosney <amr.hosni@hmaserv.com>
2021-03-04 19:35:37 +0200

    close curly prace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-
     {
         if (env('APP_ENV') != 'local') {
             URL::forceScheme('https');
-
+        }
     }
 }
