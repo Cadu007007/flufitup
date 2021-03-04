8661c3eb4a05fb168ef730f98ed0caa9fb707ffa
Mohamed Salah <sala7thedeveloper101@gmail.com>
2021-03-04 23:42:09 +0200

    fix packages empty issue

--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.admin')
 @section('content')
+{{-- {{ dd(@$packages['adhoc'] == null) }} --}}
+<p class="d-none">
+    {{ $dummy = collect()}}
+</p>
 <Admin-Packages
 title="Packages"
 date="{{Carbon\Carbon::now()->format('D, d F')}}"
-:adhocpackages="{{ @$packages['adhoc']}}"
-:biweeklypackages="{{ @$packages['bi_weekly']}}"
-:monthlypackages="{{ @$packages['monthly']}}"
+:adhocpackages="{{ @$packages['adhoc'] == null ? $dummy :  @$packages['adhoc']}}"
+:biweeklypackages="{{ @$packages['bi_weekly'] == null ? $dummy :  @$packages['bi_weekly']}}"
+:monthlypackages="{{ @$packages['monthly'] == null ? $dummy :  @$packages['monthly']}}"
 {{-- :tailoredpackages="{{ [] }}" --}}
 addpackageroute= "{{ route('admin.packages.add') }}"
 showpackageroute="{{route('admin.packages.show','package_id')}}"
