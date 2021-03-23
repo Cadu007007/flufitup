a57f1ac99225a70cb4898fb48a81c0e334d7c604
Mohamed Salah <sala7thedeveloper101@gmail.com>
2021-03-24 00:14:34 +0200

    handle landing page content responsive view except the navbar

--- resources/views/components/landing/header.blade.php
+++ resources/views/components/landing/header.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ asset('css/register/styles.css') }}">
         <link rel="stylesheet" href="{{ asset('css/login/styles.css') }}">
         <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-
-
-    @include('components.landing.header')
-    <div id="app">
-        <Landing-Page         
-        :packagesadhoclist="{{ $packages->adhoc}}"
-        :packagebiweeklylist="{{ $packages->bi_weekly}}"
-        :packagesmonthlylist="{{ $packages->monthly}}"
-        packageshowroute="{{ route('package.show', 'package_id') }}"
-        packagecreateroute="{{ route('package.create') }}"
-/>
-    </div>
-    @include('components.landing.footer')
+@include('components.landing.header')
+<div id="app">
+    <Landing-Page :packagesadhoclist="{{ $packages->adhoc }}" :packagebiweeklylist="{{ $packages->bi_weekly }}"
+        :packagesmonthlylist="{{ $packages->monthly }}" packageshowroute="{{ route('package.show', 'package_id') }}"
+        packagecreateroute="{{ route('package.create') }}" />
+</div>
+@include('components.landing.footer')
